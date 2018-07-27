eaf59750a46d58dda058cb94e4affcaa16ee724f
blackflux <blackflux@unfine.de>
2018-07-26 20:33:53 -0700

    fix: updated dependencies fix: auto release pr

--- src/https.js
+++ src/https.js
@@ -3,12 +3,12 @@ const api = require('lambda-serverless-api')({});
 
 module.exports.sum = api.wrap("GET sum", [
   api.Str("input")
-], process.env.RATE_LIMIT, ([inputString]) => {
-  const input = JSON.parse(inputString);
-  if (!(input instanceof Array) || input.some(Number.isNaN)) {
+], process.env.RATE_LIMIT, ({ input }) => {
+  const inputParsed = JSON.parse(input);
+  if (!(inputParsed instanceof Array) || inputParsed.some(Number.isNaN)) {
     throw api.ApiError("Please provide input query parameter list of numbers!");
   }
-  return api.JsonResponse({ result: input.reduce((a, b) => a + b, 0) });
+  return api.JsonResponse({ result: inputParsed.reduce((a, b) => a + b, 0) });
 });
 
 
