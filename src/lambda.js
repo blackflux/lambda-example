15818e3c7cb48c09a34861fe215b6b62583d43a5
Lukas Siemon <lukas.siemon@gmail.com>
2019-02-17 10:12:11 -0800

    fix: moved to blackflux

--- src/lambda.js
+++ src/lambda.js
@@ -1,8 +1,8 @@
-const request = require("request");
+const request = require('request');
 const moment = require('moment-timezone');
 const rollbar = require('lambda-rollbar')({
-  verbose: process.env.ROLLBAR_VERBOSE === "1",
-  enabled: process.env.ROLLBAR_ENABLED === "1",
+  verbose: process.env.ROLLBAR_VERBOSE === '1',
+  enabled: process.env.ROLLBAR_ENABLED === '1',
   accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
   environment: process.env.STAGE
 });
@@ -10,14 +10,14 @@ const rollbar = require('lambda-rollbar')({
 // Compute a / b. The function will throw an error if parameter is missing. Returns null if b is 0.
 module.exports.div = (event, context, cb) => {
   if (event.a === undefined || event.b === undefined) {
-    return cb("Parameter a and b are required.");
+    return cb('Parameter a and b are required.');
   }
   return cb(null, event.a / event.b);
 };
 
 // Returns the geoip information for the executing function IP. Request is recorded and replayed.
 module.exports.geoip = (event, context, cb) => {
-  request.get("http://ip-api.com/json", { json: true }, (err, res, body) => cb(err, body));
+  request.get('http://ip-api.com/json', { json: true }, (err, res, body) => cb(err, body));
 };
 
 // Returns the current time (UTC).
@@ -28,9 +28,9 @@ module.exports.now = (event, context, cb) => {
 // Returns null and logs an error to rollbar. When re-recording always be careful not to
 // leak information in the recorded cassettes (e.g. the rollbar access token).
 // In this case the token was changed after the recording and the cassette truncated to a minimum.
-// To catch all errors and log them without having to wrap all your functions, consider using simlu/lambda-monitor
+// To catch all errors and log them without having to wrap all your functions, consider using blackflux/lambda-monitor
 module.exports.logger = rollbar.wrap((event, context, rb) => {
-  rb.error(new Error("Some Log Message"));
+  rb.error(new Error('Some Log Message'));
   return Promise.resolve();
 });
 
