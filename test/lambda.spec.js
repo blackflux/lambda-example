dee16cf357eb50526bd7e51c2ce37a2592d1f14a
Jeff Soloshy <MacMcIrish@users.noreply.github.com>
2019-03-04 10:27:51 -0800

    fix: fixed verbose flag

--- test/lambda.spec.js
+++ test/lambda.spec.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const lambdaTester = require('lambda-tdd')({
   cwd: path.join(__dirname, '..'),
-  verbose: process.argv.slice(2).indexOf('--debug') !== -1,
+  verbose: process.argv.slice(2).indexOf('--verbose') !== -1,
   handlerFile: path.join(__dirname, '..', 'src', 'lambda.js'),
   cassetteFolder: path.join(__dirname, 'lambda', '__cassettes'),
   envVarYml: path.join(__dirname, 'env_lambda.yml'),
